9659fd46c22522d505781221686417a46bd47abf
syed habeeb <yespackindiaceo@gmail.com>
2025-07-25 07:14:10 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/documents/page.tsx
+++ src/app/documents/page.tsx
@@ -1,3 +1,6 @@
+
+'use client';
+
 import { AppHeader } from "@/components/app-header";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
